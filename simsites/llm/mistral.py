cc8fd84e244128dccb9ff3eb66c6474ebc50aa0e
Chris Coughlin <chriscoughlin@gmail.com>
2024-06-22 10:42:52 -0500

    Dropped extraneous string

--- simsites/llm/mistral.py
+++ simsites/llm/mistral.py
@@ -73,22 +73,6 @@ And here are the most relevant excerpts from your client's current web site.
 {excerpts}
 '''
 
-'''
-curl https://api.openai.com/v1/chat/completions   -H "Content-Type: application/json"   -H "Authorization: Bearer $OPENAI_API_KEY"   -d '{
-    "model": "gpt-3.5-turbo",
-    "messages": [
-      {
-        "role": "system",
-        "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."
-      },
-      {
-        "role": "user",
-        "content": "Compose a poem that explains the concept of recursion in programming."
-      }
-    ]
-  }'
-'''
-
 
 def gen_json_response_message(content: AnyStr) -> Dict:
     """
